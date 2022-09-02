d72980ed5c6e2bf78ab1872c153686326fbca367
Lee-Sang-Beom <tkdqja975@naver.com>
2022-09-02 18:25:07 +0900

    add readme.md, stopwatch : add func - stop, reset

--- 스톱워치/script.js
+++ 스톱워치/script.js
@@ -8,6 +8,9 @@ const btn_start = document.getElementById("btn_start");
 const btn_stop = document.getElementById("btn_stop");
 const btn_reset = document.getElementById("btn_reset");
 
+// setInterval 동작을 관리하기 위해 해당 함수가 반환하는 ID를 저장하는 변수
+let intervalId;
+
 // 10ms마다 수행 : 타이머 증가
 function operTimer(){
 
@@ -34,7 +37,32 @@ function operTimer(){
     }
 }
 
+// start stopwatch
 btn_start.addEventListener("click", () => {
+
+  // start 키를 두번 눌렀을 때 발생하는 오류 방지: setInterval의 반복으로 id가 겹칠 수 있음. 기존 id는 제거함
+  clearInterval(intervalId);
+
   // 10ms마다 특정 1번째 인자의 함수가 동작하도록 한다.
-  setInterval(operTimer, 10);
+  intervalId = setInterval(operTimer, 10);
+});
+
+// stop stopwatch
+btn_stop.addEventListener("click", () => {
+    // operTimer의 실행 종료
+    clearInterval(intervalId);
+});
+  
+// reset stopwatch
+btn_reset.addEventListener("click", () => {
+
+    clearInterval(intervalId);
+
+    minutes = 0;
+    seconds = 0;
+    tenMillis = 0;
+    
+    span_minute.textContent = `0${minutes}`;
+    span_second.textContent = `0${seconds}`;
+    span_tenMillis.textContent = `0${tenMillis}`;
 });
